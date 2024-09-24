819a0e057a5ed6780ff9e6220a7ff9de18ff2b05
Gustavo-F-E <112873809+Gustavo-F-E@users.noreply.github.com>
2024-09-24 15:50:31 -0300

    pwa-ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,53 @@
 import React from 'react'; // Asegúrate de importar React
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "next/head"; // Importar el componente Head
-//import getManifest from './manifest';
+import type { Metadata, Viewport } from "next";
+
+//const APP_NAME = "Filament Path Generator";
+//const APP_DEFAULT_TITLE = "Filament Path Generator";
+//const APP_TITLE_TEMPLATE = "%s - PWA App";
+//const APP_DESCRIPTION = "Best PWA app in the world!";
+
+export const metadata: Metadata = {
+  applicationName: "Filament Path Generator",
+  title: {
+    default: "Filament Path Generator",
+    template: "Filament Path Generator"
+  },
+  description: "Creada por Gustavo Francisco Eichhorn",
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Filament Path Generator",
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: "Filament Path Generator",
+    title: {
+      default: "Filament Path Generator",
+      template: "Filament Path Generator",
+    },
+    description: "Creada por Gustavo Francisco Eichhorn",
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: "Filament Path Generator",
+      template: "Filament Path Generator",
+    },
+    description: "Creada por Gustavo Francisco Eichhorn",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,11 +60,6 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "Filament Path Generator",
-  description: "Creada por Gustavo Francisco Eichhorn",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,33 +67,6 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
-      <Head>
-        {/*  <meta name="theme-color" content="#F0DB4F">
-  <meta name="MobileOptimized" content="width">
-  <meta name="HandheldFriendly" content="true">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-  <link rel="shortcut icon" type="image/png" href="./img/ProgramadorFitness.png">
-  <link rel="apple-touch-icon" href="./ProgramadorFitness.png">
-  <link rel="apple-touch-startup-image" href="./ProgramadorFitness.png">
-  <link rel="manifest" href="./manifest.json">*/}
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="MobileOptimized" content="width"/>
-        <meta name="HandheldFriendly" content="true" />
-        <meta name="apple-mobile-web-app-capable" content="yes"/>
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
-        <link rel="icon" href="./icon.ico" sizes="16x16" type="image/png" />
-        {/*<link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png"/>*/}
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="apple-touch-startup-image" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
-        <link rel="icon" type="image/png" sizes="144x144" href="/android-chrome-144x144.png" />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
