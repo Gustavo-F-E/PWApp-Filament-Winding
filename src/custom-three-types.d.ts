4be7a692a3c8f75c93e9d2de3b3ec5e388ad4481
Gustavo-F-E <112873809+Gustavo-F-E@users.noreply.github.com>
2024-12-19 08:26:18 -0300

    package error

--- src/custom-three-types.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'three/examples/jsm/controls/OrbitControls' {
-  export class OrbitControls {
-    constructor(object: THREE.Camera, domElement: HTMLElement);
-    update(): void;
-    enableDamping: boolean;
-    dampingFactor: number;
-  }
-}
